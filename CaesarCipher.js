7bc29f5f25071126c509e099cffbd55a424a41e3
AleksiDu <aleksi.duluzauri@gmail.com>
2022-04-04 21:53:29 +0400

    v2

--- CaesarCipher.js
+++ CaesarCipher.js
@@ -1,11 +1,13 @@
-const input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-let encode = "";
-
-let str = "adcd";
-
-for (let i = 0; i < str.length; i++){
-    const index = input.indexOf(str[i]);
-    encode += output[index];
-}
+function rot13(str) {
+    let encode = "";
+    const input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!?. ";
+    const output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm!?. ";
+  
+    for (let i = 0; i < str.length; i++){
+      const index = input.indexOf(str[i]);
+      encode += output[index];
+  }
+  return encode;
+  }
+  
+  console.log(rot13("SERR PBQR PNZC"));
